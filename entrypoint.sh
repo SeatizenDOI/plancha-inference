d2605c4f3cfd1361212db5b6a9563e261932df86
Gouderg <victor.illien974@gmail.com>
2024-08-15 03:01:52 +0400

    Base docker script

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Check for environment variable and set command accordingly
+if [[ -n ${index_pos} ]]; then
+  COMMAND="python inference.py -efol -pfol /plancha -jgpu -mlgpu -c -ip $index_pos"
+elif [[ -n ${index_start} ]]; then
+  COMMAND="python inference.py -efol -pfol /plancha -jgpu -mlgpu -c -is $index_start"
+else
+  COMMAND="python inference.py -efol -pfol /plancha -jgpu -mlgpu -c"
+fi
+
+# Execute the command
+exec $COMMAND
